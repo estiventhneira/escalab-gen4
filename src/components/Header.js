5a797b30ed3fe987548444f0e77259dd4595a449
estiventhneira <estiventhneira@gmail.com>
2021-11-03 23:31:10 -0500

    init

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+const Header = () => {
+  console.log('Hola Mundo');
+  return (
+    <View
+      style={{
+        height: 100,
+        backgroundColor: 'black',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 15,
+      }}>
+      <Text style={{color: 'white', fontSize: 25, fontWeight: '800'}}>
+        Bienvenido
+      </Text>
+      <View>
+        <Image
+          source={require('../assets/Tesla-Emblema.png')}
+          style={{height: 90, width: 90}}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Header;
