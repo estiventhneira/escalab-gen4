74f519c6fdfaaa39c5aaf45d03bc7dd56d423931
estiventhneira <estiventhneira@gmail.com>
2021-11-23 19:39:05 -0500

    feat: delete character

--- src/screens/SpaceX/index.js
+++ src/screens/SpaceX/index.js
@@ -12,24 +12,25 @@ import axios from 'axios';
 import {gql, useQuery} from '@apollo/client';
 import {useNavigation} from '@react-navigation/native';
 
-const CHARACTERSBYIDS = gql`
-  query characters {
-    charactersByIds(ids: [1, 2, 3, 4, 5, 6, 7, 8]) {
-      name
-      id
-      image
-    }
-  }
-`;
-
 const index = () => {
   const navigation = useNavigation();
+  const [characters, setCharacters] = useState([1, 2, 3, 4, 5]);
+
+  const CHARACTERSBYIDS = gql`
+    query characters($ids: [ID!]!) {
+      charactersByIds(ids: $ids) {
+        name
+        id
+        image
+      }
+    }
+  `;
 
   const {
     error: errorLocation,
     data: dataLocation,
     loading: loadingLocation,
-  } = useQuery(CHARACTERSBYIDS);
+  } = useQuery(CHARACTERSBYIDS, {variables: {ids: characters}});
 
   return (
     <SafeAreaView style={{padding: 15}}>
@@ -42,29 +43,65 @@ const index = () => {
         <Text>Screen de Space X</Text>
 
         {loadingLocation ? <Text>...</Text> : <Text>Data</Text>}
-        {errorLocation ? <Text>Hubo un error de conexión</Text> : null}
+        {errorLocation ? console.log(errorLocation) : null}
 
         {dataLocation &&
           dataLocation.charactersByIds.map(item => {
             return (
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('ProductDetailsRandM', {id: item.id});
-                }}
-                style={{
-                  marginBottom: 30,
-                }}>
-                <Image
-                  source={{uri: item.image}}
+              <View style={{width: '100%'}}>
+                <TouchableOpacity
                   style={{
-                    height: 300,
-                    width: '90%',
-                    marginVertical: 20,
-                    borderRadius: 10,
+                    alignItems: 'center',
+                    width: '100%',
+                    marginBottom: 30,
                   }}
-                />
-                <Text>{item.name}</Text>
-              </TouchableOpacity>
+                  onPress={() => {
+                    navigation.navigate('ProductDetailsRandM', {id: item.id});
+                  }}>
+                  <Image
+                    source={{uri: item.image}}
+                    style={{
+                      height: 300,
+                      width: '90%',
+                      marginVertical: 20,
+                      borderRadius: 10,
+                    }}
+                  />
+                  <Text>{item.name}</Text>
+                </TouchableOpacity>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    marginBottom: 15,
+                    justifyContent: 'space-around',
+                  }}>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: 'green',
+                      width: 150,
+                      padding: 10,
+                      borderRadius: 5,
+                    }}>
+                    <Text style={{color: 'white'}}>Comprar</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => {
+                      const filtered = characters.filter(function (element) {
+                        return element != item.id;
+                      });
+                      setCharacters(filtered);
+                    }}
+                    style={{
+                      backgroundColor: 'red',
+                      width: 150,
+                      padding: 10,
+                      borderRadius: 5,
+                    }}>
+                    <Text style={{color: 'white'}}>Eliminar</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
             );
           })}
       </ScrollView>
